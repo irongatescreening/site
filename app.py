dcc9c5103a9b37038f7e38ac75b3d0416da03d10
Iron Gate Screening <irongatescreening@gmail.com>
2026-01-13 10:07:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,11 +13,12 @@ import sqlite3
 from datetime import datetime, timezone
 
 import requests
-from flask import Flask, request, session, redirect, url_for
+from flask import Flask, request, session, redirect, url_for, jsonify
 
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_talisman import Talisman
+
 
 
 # -------------------------------------------------
